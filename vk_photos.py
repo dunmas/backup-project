9bdd1062b94450f6745b9ef90aa5bcc66add479b
Danila <levafork@gmail.com>
2022-12-03 15:14:07 +0300

    Начал писать метод получения фотографий профиля, теперь получаем JSON с информацией фоток и ссылками разных размеров

--- vk_photos.py
+++ vk_photos.py
@@ -7,8 +7,19 @@ class VkPhotos:
     """
     Класс работы с фотографиями профиля по переданному user_id
     """
-    def __init__(self, access_token, user_id, version='5.131'):
-        self.token = access_token
+    def __init__(self, user_id, version='5.131'):
+        self.token = VK_TOKEN
         self.id = user_id
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
+
+    def get_profile_photos(self):
+        url = 'https://api.vk.com/method/photos.get'
+        params = {'owner_id': self.id,
+                  'album_id': 'profile',
+                  'photo_sizes': 1,
+                  'extended': 1}
+
+        # Получили JSON ответа сервера с фотографиями профиля
+        response = requests.get(url, params={**self.params, **params}).json()
+
