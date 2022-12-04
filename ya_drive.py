714802b6046be9e80ab726b2b1f88f263107f4f9
Danila <levafork@gmail.com>
2022-12-04 23:13:34 +0300

    Реализовал метод загрузки фотографии в папку на Яндекс.Диске. Начал писать метод создания директории на Диске для загрузки

--- ya_drive.py
+++ ya_drive.py
@@ -6,14 +6,17 @@ class YaDrive:
     def __init__(self, dir_name='VK_Profile_Backup'):
         self.headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {YA_TOKEN}'}
         self.base_url = 'https://cloud-api.yandex.net/'
-        self.drive_dir = dir_name
+        self._make_drive_dir(dir_name)
 
-    def _make_drive_dir(self):
+
+    def _make_drive_dir(self, dir_name):
         """
-        Создаёт на Яндекс.Диске папку с названием из self.drive_dir
+        Создаёт на Яндекс.Диске папку с названием dir_name, если такая есть - удаляет существующую и пишет
+        новую
+        :param dir_name: Имя создаваемой на диске директории
         :return:
         """
-        pass
+        self.drive_dir = dir_name
 
     def upload_by_url(self, url):
         """
@@ -21,4 +24,10 @@ class YaDrive:
         :param url: Ссылка на загрузку фотографии
         :return:
         """
-        pass
+        uri = '/v1/disk/resources/upload'
+        request_url = self.base_url + uri
+        params = {'url': url, 'path': self.drive_dir}
+        response = requests.post(request_url, params=params, headers=self.headers)
+
+        if response.status_code == 201:
+            print('Готово!')
