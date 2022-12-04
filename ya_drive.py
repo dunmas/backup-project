9eeaa60909dea6c90df84e7c310f9dbe2f93c903
Danila <levafork@gmail.com>
2022-12-04 23:03:12 +0300

    Подготовил заготовки для основной логики класса YaDrive

--- ya_drive.py
+++ ya_drive.py
@@ -3,8 +3,22 @@ import requests
 from settings import YA_TOKEN
 
 class YaDrive:
-    def __init__(self):
+    def __init__(self, dir_name='VK_Profile_Backup'):
         self.headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {YA_TOKEN}'}
         self.base_url = 'https://cloud-api.yandex.net/'
+        self.drive_dir = dir_name
 
+    def _make_drive_dir(self):
+        """
+        Создаёт на Яндекс.Диске папку с названием из self.drive_dir
+        :return:
+        """
+        pass
 
+    def upload_by_url(self, url):
+        """
+        Загружает на диск фото по ссылке в папку из self.drive_dir
+        :param url: Ссылка на загрузку фотографии
+        :return:
+        """
+        pass
