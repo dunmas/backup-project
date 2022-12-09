99354912b499775002509a601b62d1be916e0f24
Danila <levafork@gmail.com>
2022-12-09 22:24:06 +0300

    Обновка - замена Progress на tqdm, чтобы правильно работал. Небольшие принты для вывода текущего статуса

--- vk_photos.py
+++ vk_photos.py
@@ -1,8 +1,9 @@
+from datetime import datetime
+
+from tqdm import tqdm
 import requests
 
 from settings import VK_TOKEN
-from datetime import datetime
-from progress.bar import IncrementalBar
 
 
 class VkPhotos:
@@ -24,15 +25,13 @@ class VkPhotos:
         :param pictures: список фотографий, полученный get запросом
         :return: none
         """
-        bar = IncrementalBar('Сортируем ваши фото...', max=len(pictures))
-        for image in pictures:
+        print('Обрабатываем полученные фотографии...')
+        for image in tqdm(pictures, colour='GREEN'):
             if image['likes']['count'] in self.photos:
                 date = datetime.utcfromtimestamp(image['date']).strftime('%Y-%m-%d')
                 self.photos[image['likes']['count'] + date] = self._get_max_def_link(image)
             else:
                 self.photos[image['likes']['count']] = self._get_max_def_link(image)
-
-            bar.next()
 
     def _get_max_def_link(self, picture):
         """
@@ -58,6 +57,7 @@ class VkPhotos:
                   'album_id': 'profile',
                   'photo_sizes': 1,
                   'extended': 1}
+        print('Смотрим ваш профиль...')
 
         # Получили JSON ответа сервера с фотографиями профиля - фото - response['response']['items']
         response = requests.get(url, params={**self.params, **params}).json()
